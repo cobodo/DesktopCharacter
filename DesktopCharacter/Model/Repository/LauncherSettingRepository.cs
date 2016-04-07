7db8127039e42a564e4dd2595483c706eb2dcf20
orekyuu <orekyuu@yahoo.co.jp>
2016-04-08 00:30:24 +0900

    実装TANOMU

---
+++ DesktopCharacter/Model/Repository/LauncherSettingRepository.cs
@@ -0,0 +1,23 @@
+﻿using DesktopCharacter.Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopCharacter.Model.Repository
+{
+    public class LauncherSettingRepository
+    {
+        public void save(LauncherSetting settings)
+        {
+            //TODO データをjson形式で書き出し、すでに存在すれば上書きする
+        }
+
+        public LauncherSetting load()
+        {
+            //TODO ローカルのデータを読み込み、存在しなければnew LauncherSettings()を返す
+            return null;
+        }
+    }
+}
