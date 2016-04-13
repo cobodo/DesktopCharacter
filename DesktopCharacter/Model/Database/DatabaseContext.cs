afb51d088513738876f7adcdee813ef995cb8ccd
orekyuu <orekyuu@yahoo.co.jp>
2016-04-13 17:40:10 +0900

    SQLite対応

---
+++ DesktopCharacter/Model/Database/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using SQLite.CodeFirst;
+using System.Data.Entity;
+
+
+namespace DesktopCharacter.Model.Database
+{
+    class DatabaseContext: DbContext
+    {
+        public DatabaseContext(): base("name=mydatabase")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<DatabaseContext>(modelBuilder);
+            System.Data.Entity.Database.SetInitializer(sqliteConnectionInitializer);
+        }
+    }
+}
---
+++ DesktopCharacter/Model/DatabaseModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopCharacter.Model.Domain;
+using System.IO;
+using System.Data;
+
+
+namespace DesktopCharacter.Model
+{
+    class DatabaseModel
+    {
+        public static DatabaseModel Instance { get; } = new DatabaseModel();
+
+        private DatabaseModel()
+        {
+        }
+
+        public void Innitialize()
+        {
+            var conf = new SQLiteConnectionStringBuilder()
+            {
+                DataSource = "test.db"
+            };
+            var connection = new SQLiteConnection(conf.ToString());
+            var context = new DataContext(connection);
+            context.Database.CreateIfNotExists();
+        }
+    }
+
+    public class DataContext: DbContext
+    {
+        public DbSet<TwitterAccount> TwitterAccounts { get; set; }
+
+        public DataContext(SQLiteConnection connection): base()
+        {
+        }
+    }
+}
