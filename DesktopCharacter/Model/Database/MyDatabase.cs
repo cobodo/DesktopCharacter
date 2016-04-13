a0749ee8fa0c2a095027a9d8d5b6a19f535ad2e9
orekyuu <orekyuu@gmail.com>
2016-04-13 15:22:03 +0900

    babumi

---
+++ DesktopCharacter/Model/Database/MyDatabase.cs
@@ -0,0 +1,27 @@
+﻿using DesktopCharacter.Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.CodeFirst;
+
+namespace DesktopCharacter.Model.Database
+{
+    class MyDatabase: DbContext
+    {
+        public MyDatabase(): base("name=mydatabase")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<MyDatabase>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+        }
+    }
+}
---
+++ DesktopCharacter/Model/Domain/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopCharacter.Model.Domain
+{
+    [Table("user")]
+    class User
+    {
+        [Column("user_id")]
+        public int Id { get; set; }
+        [Column("user_name")]
+        public string Name { get; set;  }
+    }
+}
