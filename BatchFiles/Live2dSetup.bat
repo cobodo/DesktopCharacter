8feb1aada14b420722da3098c1213188df15e489
kirisamemarisa <marisa.love0925@gmail.com>
2016-06-22 01:41:43 +0900

    [modify]やっぱりファイル名変更

--- BatchFiles/Live2dSetup.bat
+++ BatchFiles/Live2dSetup.bat
@@ -1,12 +1,11 @@
 echo on
 
-SET SDKPath='F:\Live2D_SDK_OpenGL_2.0.08_2_jp'
-SET Platform='x64'
-SET VsVersion='120'
-SET Output='%~dp0..\Live2D for DLL\Dependency\'
+SET SDKPath=F:\Live2D_SDK_OpenGL_2.0.08_2_jp
+SET Platform=x64
+SET VsVersion=120
+SET Output=%~dp0..\live2dfordll\Dependency
 
 echo %Output%
-cd Impl
-call Live2dSetup.exe %SDKPath% %Platform% %VsVersion% %Output%
+call python ..\tool\Live2dSetup.py %SDKPath% %Platform% %VsVersion% %Output%
 
 pause
---
+++ Tool/Live2dSetup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import shutil
+
+# 引数1 : Live2DのSDKフォルダーパス
+# 引数2 : Platform
+# 引数3 : vsVersion
+# 引数4 : Live2DForDLLの依存フォルダーのパス
+
+sdkPath = sys.argv[ 1 ]
+platform = sys.argv[ 2 ]
+vsVersion = sys.argv[ 3 ]
+outputPath = sys.argv[ 4 ]
+
+Live2DFolder = "Live2D_SDK_OpenGL"
+include = "include"
+lib = "lib"
+
+print( "path check" )
+print( "sdkPath = %s" ) % ( sdkPath )
+print( "platform = %s" ) % ( platform )
+print( "vsVersion = %s" ) % ( vsVersion )
+print( "outputPath = %s" ) % ( outputPath )
+
+# 一気にフォルダー作れないので
+try:
+    os.mkdir(outputPath)
+    os.mkdir(outputPath + "\\" + Live2DFolder)
+    os.mkdir(outputPath + "\\" + Live2DFolder + "\\" + include)
+    os.mkdir(outputPath + "\\" + Live2DFolder + "\\" + lib)
+except OSError:
+    pass
+    
+# コピー開始
+# includeコピー
+shutil.copytree(sdkPath + "\\" + include, outputPath + "\\" + Live2DFolder + "\\" + include)
+# libコピー
+shutil.copytree(sdkPath + "\\" + lib + "\\" + "windows" + "\\" + platform + "\\" + vsVersion, outputPath + "\\" + Live2DFolder + "\\" + lib)
+
