c4c7d6db3100c3f2c6e07be1c4c35018da35a062
kirisamemarisa <marisa.love0925@gmail.com>
2016-06-22 03:17:17 +0900

    [modify]live2dfordll -> Extension へ名前を変更 [modify]nuget.exeを追加 Msbuildでビルドできるように

--- BatchFiles/CreateLive2dfordll.bat
+++ BatchFiles/CreateLive2dfordll.bat
@@ -1,6 +1,8 @@
 SET MSBUILD="C:/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe"
+SET NUGET="%~dp0../live2dfordll/.nuget/NuGet.exe"
 SET EnableNuGetPackageRestore=true
 
+call %NUGET% restore "%~dp0../live2dfordll/Live2D for DLL.sln"
 call %MSBUILD% "%~dp0../live2dfordll/Live2D for DLL.sln" /t:Build /p:Platform=x64 /property:Configuration=Debug
 
 pause
