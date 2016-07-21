8a0bba832f73d87135e3257a7e8e09c58a24fdcf
kirisamemarisa <marisa.love0925@gmail.com>
2016-07-22 00:00:10 +0900

    [add]追加忘れのファイル追加

---
+++ DesktopCharacter/Model/Service/TimeSignal/Impl/AnimationTImeSignal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopCharacter.Model.Service.TimeSignal.Impl
+{
+    class AnimationTImeSignal : TimeSignalServiceAdapter
+    {
+        public override void OnMessage(DateTime time)
+        {
+            CharacterNotify.Instance.SetAnimation("");
+        }
+    }
+}
---
+++ DesktopCharacter/Model/Service/TimeSignal/Impl/SimpleTimeSignalService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopCharacter.Model.Service.TimeSignal.Impl
+{
+    /// <summary>
+    /// 現在の時刻のみを表示するシンプルな時報
+    /// </summary>
+    class SimpleTimeSignalService : TimeSignalServiceAdapter
+    {
+        public override void OnMessage(DateTime time)
+        {
+            CharacterNotify.Instance.Talk(time.Hour + "時になったよ");
+        }
+    }
+}
